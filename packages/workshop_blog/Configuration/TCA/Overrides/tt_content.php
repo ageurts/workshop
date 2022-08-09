a51e0e601b4be23abb250b91ab9db28789762e52
A. Geurts <ageurts@users.noreply.github.com>
2022-08-09 12:30:15 +0200

    Update tt_content.php
    ; -> ; typo :)

--- packages/workshop_blog/Configuration/TCA/Overrides/tt_content.php
+++ packages/workshop_blog/Configuration/TCA/Overrides/tt_content.php
@@ -22,7 +22,7 @@ call_user_func(function (): void {
     );
     
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['workshopblog_list'] = 'pi_flexform';
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility;:addPiFlexFormValue('workshopblog_list', 'FILE:EXT:workshop_blog/Configuration/Flexforms/Flexform.xml');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('workshopblog_list', 'FILE:EXT:workshop_blog/Configuration/Flexforms/Flexform.xml');
     
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['workshopblog_latest'] = 'pi_flexform';
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('workshopblog_latest', 'FILE:EXT:workshop_blog/Configuration/Flexforms/Flexform.xml');
